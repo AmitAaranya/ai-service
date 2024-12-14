400c4c47f630e8404242b86e7b3dcc7126e2a406
AMIT <amitaaranya@gmail.com>
2024-12-15 01:11:59 +0530

    Created UI page

--- ui/app.py
+++ ui/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -6,8 +7,19 @@ app = Flask(__name__)
 AI_BACKEND_URL = "http://ai-backend:5001"
 
 @app.route("/")
-def hello():
-    return "Hello from UI Flask Server"
+def home():
+    return render_template("home.html")
+
+@app.post("/detect")
+def detect():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file found'}), 400
+    
+    file = request.files['image']
+    file_path = os.path.join("static", file.filename)
+    file.save(file_path)
+    
+    return jsonify({"status": 0,"data": [1]})
 
 
 @app.get("/check-ai-backend")
